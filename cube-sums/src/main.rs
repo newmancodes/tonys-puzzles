70229278af2bfdab698021be9b2388c73a3a50b8
Stephen Newman <steve@newman.digital>
2021-03-23 21:05:24 +0000

    Less Awful.

--- cube-sums/src/main.rs
+++ cube-sums/src/main.rs
@@ -1,12 +1,19 @@
 mod cube_sum_finder;
 mod awful_cube_sum_finder;
+mod slightly_less_awful_cube_sum_finder;
+mod less_awful_cube_sum_finder;
 
 use cube_sum_finder::CubeSumFinder;
 use awful_cube_sum_finder::AwfulCubeSubFinder;
+use slightly_less_awful_cube_sum_finder::SlightlyLessAwfulCubeSubFinder;
+use less_awful_cube_sum_finder::LessAwfulCubeSubFinder;
+
+extern crate num_integer;
+
 use stopwatch::Stopwatch;
 
 fn main() {
-    let finder = AwfulCubeSubFinder {};
+    let finder = LessAwfulCubeSubFinder {};
     let mut sw = Stopwatch::start_new();
     let found_cube_sums = finder.find_cube_sums(100);
     let elapsed = sw.elapsed();
